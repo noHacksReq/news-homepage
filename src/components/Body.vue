3a6d3147da4b0130de643c1956d60ce922c36a0a
Tor Rydberg <crownroyalsuperslut@yahoo.com>
2023-10-10 11:20:37 -0400

    added files, assets, and did initial set up

---
+++ src/components/Body.vue
@@ -0,0 +1,19 @@
+<script setup>
+
+</script>
+
+<template>
+ <section>Main content goes here</section>
+</template>
+
+<style scoped>
+section {
+    background-color: var(--softOrange);
+}
+
+
+
+@media (min-width: 1024px) {
+
+}
+</style>
